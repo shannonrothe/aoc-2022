15f8deca100b14c74003a970f100dc688698c3b9
Shannon Rothe <shannon.michael.rothe@gmail.com>
2022-12-05 21:50:44 +1100

    cleanup day 5

--- day_5/main.go
+++ day_5/main.go
@@ -27,6 +27,10 @@ func (c *crate) pop() byte {
 	return 0
 }
 
+func (c *crate) join(o *crate) {
+	c.stack = append(c.stack, o.stack...)
+}
+
 func (c *crate) moveIndividualToN(o *crate, n int) {
 	for i := 0; i < n; i++ {
 		b := c.pop()
@@ -42,9 +46,7 @@ func (c *crate) moveToN(o *crate, n int) {
 		s.stack = append(s.stack, c.pop())
 	}
 	s.reverse()
-	for _, i := range s.stack {
-		o.push(i)
-	}
+	o.join(&s)
 }
 
 func (c *crate) reverse() {
@@ -57,71 +59,68 @@ type command struct {
 	n, f, t int
 }
 
-func parseInput(lines []string) ([9]crate, []command) {
-	crates := [9]crate{}
-	commands := []command{}
+func parseMovement(str string) (int, int, int) {
+	line := strings.Split(str, " ")
+	n, _ := strconv.Atoi(line[1])
+	f, _ := strconv.Atoi(line[3])
+	t, _ := strconv.Atoi(line[5])
+	return n, f - 1, t - 1
+}
 
-	for i := 0; i < 8; i++ {
-		line := lines[i]
+func parseInput(crates []string, cmd []string) ([9]crate, []command) {
+	crs := [9]crate{}
+	cmds := []command{}
 
+	for _, line := range crates {
 		for j := 0; j < len(line); j += 3 {
 			if j != 0 {
 				j += 1
 			}
-
-			if string(line[j]) == "[" {
-				crates[j/4].push(line[j+1])
+			if line[j] == byte('[') {
+				crs[j/4].push(line[j+1])
 			}
 		}
 	}
 
-	for _, c := range crates {
+	for _, c := range crs {
 		c.reverse()
 	}
 
-	for i := 10; i < len(lines); i++ {
-		line := strings.Split(lines[i], " ")
-		n, _ := strconv.Atoi(line[1])
-		f, _ := strconv.Atoi(line[3])
-		t, _ := strconv.Atoi(line[5])
-		commands = append(commands, command{
-			n: n,
-			f: f - 1,
-			t: t - 1,
-		})
+	for _, cmd := range cmd {
+		n, f, t := parseMovement(cmd)
+		cmds = append(cmds, command{n, f, t})
 	}
 
-	return crates, commands
+	return crs, cmds
 }
 
-func part1(lines []string) string {
-	crates, commands := parseInput(lines)
-	for _, cmd := range commands {
-		crates[cmd.f].moveIndividualToN(&crates[cmd.t], cmd.n)
-	}
-
+func top(crs [9]crate) string {
 	tops := []string{}
-	for _, c := range crates {
+	for _, c := range crs {
 		tops = append(tops, string(c.pop()))
 	}
 	return strings.Join(tops, "")
 }
 
-func part2(lines []string) string {
-	crates, commands := parseInput(lines)
-	for _, cmd := range commands {
-		crates[cmd.f].moveToN(&crates[cmd.t], cmd.n)
+func part1(crs [9]crate, cmds []command) string {
+	for _, cmd := range cmds {
+		crs[cmd.f].moveIndividualToN(&crs[cmd.t], cmd.n)
 	}
+	return top(crs)
+}
 
-	tops := []string{}
-	for _, c := range crates {
-		tops = append(tops, string(c.pop()))
+func part2(crs [9]crate, cmds []command) string {
+	for _, cmd := range cmds {
+		crs[cmd.f].moveToN(&crs[cmd.t], cmd.n)
 	}
-	return strings.Join(tops, "")
+	return top(crs)
 }
 
 func main() {
-	lines := strings.Split(input, "\n")
-	fmt.Printf("part 1: %s\n", part1(lines))
-	fmt.Printf("part 2: %s\n", part2(lines))
+	parts := strings.Split(input, "\n\n")
+	crates, commands := parseInput(strings.Split(parts[0], "\n"), strings.Split(parts[1], "\n"))
+	fmt.Printf("part 1: %s\n", part1(crates, commands))
+
+	crates, commands = parseInput(strings.Split(parts[0], "\n"), strings.Split(parts[1], "\n"))
+	fmt.Printf("part 2: %s\n", part2(crates, commands))
 }
